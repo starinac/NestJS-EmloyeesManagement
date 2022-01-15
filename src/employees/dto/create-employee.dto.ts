1251e6ec89736931018ef3cf8a6af5b03fc6f49c
Nikola <starinac12@gmail.com>
2022-01-15 23:32:34 +0100

    added validation

--- src/employees/dto/create-employee.dto.ts
+++ src/employees/dto/create-employee.dto.ts
@@ -1,11 +1,23 @@
+import { IsDateString, IsEmail, IsNumber, IsOptional, IsPhoneNumber, IsString } from "class-validator";
+
 export class CreateEmployeeDto {
+    @IsString()
     readonly name: string;
+    @IsEmail()
     readonly email: string;
+    @IsPhoneNumber()
     readonly phoneNumber: string;
+    @IsString()
     readonly city: string;
+    @IsNumber()
     readonly zip: number;
+    @IsString()
     readonly adressLine1: string;
+    @IsOptional()
+    @IsString()
     readonly adressLine2: string;
+    @IsDateString()
     readonly dateOfEmployment: Date;
+    @IsDateString()
     readonly dateOfBirth: Date;
 }
--- src/employees/dto/update-employee.dto.ts
+++ src/employees/dto/update-employee.dto.ts
@@ -1,4 +1,24 @@
 import { PartialType } from '@nestjs/swagger';
+import { IsOptional } from 'class-validator';
 import { CreateEmployeeDto } from './create-employee.dto';
 
-export class UpdateEmployeeDto extends PartialType(CreateEmployeeDto) {}
+export class UpdateEmployeeDto extends PartialType(CreateEmployeeDto) {
+    @IsOptional()
+    readonly name: string;
+    @IsOptional()
+    readonly email: string;
+    @IsOptional()
+    readonly phoneNumber: string;
+    @IsOptional()
+    readonly city: string;
+    @IsOptional()
+    readonly zip: number;
+    @IsOptional()
+    readonly adressLine1: string;
+    @IsOptional()
+    readonly adressLine2: string;
+    @IsOptional()
+    readonly dateOfEmployment: Date;
+    @IsOptional()
+    readonly dateOfBirth: Date;
+}
